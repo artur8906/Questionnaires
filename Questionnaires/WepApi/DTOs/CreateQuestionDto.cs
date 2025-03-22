14bbe07c87774825ae917db1144ea4a56861f874
Artur Hakobyan <hakobyanartur89@gmail.com>
2025-03-22 11:05:24 +0100

    Implemented DTOs (CreateQuestionDto, SubmitResponseDto). Add Core project reference to Wep.Api.csproj Add .gitignore and exclude Visual Studio files

---
+++ Questionnaires/WepApi/DTOs/CreateQuestionDto.cs
@@ -0,0 +1,15 @@
+﻿using Core.Enums;
+
+namespace WepApi.DTOs
+{
+    public class CreateQuestionDto
+    {
+        public string Title { get; set; } = null!;
+        public QuestionType Type { get; set; }
+
+        public int? MinValue { get; set; }
+        public int? MaxValue { get; set; }
+
+        public List<string>? Options { get; set; }
+    }
+}
---
+++ Questionnaires/WepApi/DTOs/SubmitResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WepApi.DTOs
+{
+    public class SubmitResponseDto
+    {
+        public string RespondentId { get; set; } = null!;
+        public int? RatingValue { get; set; }          
+        public List<string>? SelectedOptions { get; set; } 
+        public string? SelectedOption { get; set; }  
+    }
+}
